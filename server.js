bba8819419840f5499d7f8ac58fa0b3add8c847b
dds <android_shuai@163.com>
2018-12-28 16:22:15 +0800

    初次提交

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var SkyRTC = require('skyrtc').listen(server);
+var path = require("path");
+
+var port = process.env.PORT || 3000;
+var hostname = "0.0.0.0"
+server.listen(port, hostname, function(){
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+SkyRTC.rtc.on('new_connect', function(socket) {
+	console.log('创建新连接');
+});
+
+SkyRTC.rtc.on('remove_peer', function(socketId) {
+	console.log(socketId + "用户离开");
+});
+
+SkyRTC.rtc.on('new_peer', function(socket, room) {
+	console.log("新用户" + socket.id + "加入房间" + room);
+});
+
+SkyRTC.rtc.on('socket_message', function(socket, msg) {
+	console.log("接收到来自" + socket.id + "的新消息：" + msg);
+});
+
+SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
+	console.log("接收到来自" + socket.id + "的ICE Candidate");
+});
+
+SkyRTC.rtc.on('offer', function(socket, offer) {
+	console.log("接收到来自" + socket.id + "的Offer");
+});
+
+SkyRTC.rtc.on('answer', function(socket, answer) {
+	console.log("接收到来自" + socket.id + "的Answer");
+});
+
+SkyRTC.rtc.on('error', function(error) {
+	console.log("发生错误：" + error.message);
+});
